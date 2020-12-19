1631f1d19b658ae577484995449cda7080b6ff8a
dtuk9193 <davi916uk9310@outlook.com>
2020-12-19 15:34:32 +0800

    Complete_front
    complete version of front-end

---
+++ src/redux/crisislogger/reducer/update.reducer.js
@@ -0,0 +1,41 @@
+import {
+    UPDATE_DATA,
+    UPDATE_DATA_FAILED,
+    UPDATE_DATA_SUCCESS,
+} from '../actionType'
+
+const initialState = {
+    loading: false,
+    loaded: false,
+    error: '',
+    result: ''
+}
+
+export default (state = initialState, { type, payload }) => {
+    switch(type){
+        case UPDATE_DATA:
+            return {
+                ...state,
+                loading: true,
+                loaded: false,
+            }
+        case UPDATE_DATA_SUCCESS:    
+            return {
+                ...state,
+                loading: false,
+                loaded: true,
+                result: payload
+            }
+        case UPDATE_DATA_FAILED:
+            return {
+                ...state,
+                loading: false,
+                loaded: true,
+                error: payload
+            }
+        default: 
+            return {
+                ...state
+            }
+    }
+}
